c53b3164e43c35599373bca63844b7e01cfd9bdd
Luise Punge <l.punge@proton.me>
2025-07-19 22:51:27 +0200

    update terms

--- app.vue
+++ app.vue
@@ -14,7 +14,7 @@
       <small>
         Dieses Werk ist eine Abwandlung der Webseite <a
           href="https://jackyscript.github.io/jackyscript/">jackyscript</a>, lizenziert unter
-        <a href="https://creativecommons.org/licenses/by-sa/4.0/" target="_blank" rel="noopener">CC BY-SA 4.0</a>,
+        <a href="https://creativecommons.org/licenses/by-sa/4.0/" target="_blank" rel="noopener">CC BY-SA 4.0</a>, und wurde
         erstellt mittels <a href="https://nuxt.com" target="_blank" rel="noopener">Nuxt</a> und
         <a href="https://picocss.com" target="_blank" rel="noopener">Pico CSS <svgo-spark aria-hidden="true"
             alt="Spark icon" /></a>.
